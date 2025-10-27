dd49d7b0cf089730ac34f5b9f705daa5ac04bcdc
Alumno Torre Tecnológica <alu_torre1@instructores.senati.local>
2025-10-27 11:53:05 -0500

    Solucionando los errores de Junior :V

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Post extends Model
 {
@@ -11,4 +12,10 @@ class Post extends Model
     public $timestamps = true;
 
     protected $fillable = ['title', 'content'];
+
+    public function fromDateTime($value)
+    {
+        // Convierte a un formato que SQL Server pueda leer correctamente
+        return Carbon::parse(parent::fromDateTime($value))->format('Y-d-m H:i:s');
+    }
 }
