481ade48be27cc723e54ccfc929931e129e2570e
Alumno Torre Tecnológica <alu_torre1@instructores.senati.local>
2025-10-27 11:52:26 -0500

    awewadagwfd

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Post extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'content'];
+
+    public function fromDateTime($value)
+    {
+        // Convierte a un formato que SQL Server pueda leer correctamente
+        return Carbon::parse(parent::fromDateTime($value))->format('Y-d-m H:i:s');
+    }
 }
